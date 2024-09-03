7659d5ae72f0974f3134b3df3726bbeb681e4a61
Raj <rs457036@gmail.com>
2024-09-03 18:45:06 +0530

    update user agent

--- lib/di/modules.dart
+++ lib/di/modules.dart
@@ -1,6 +1,8 @@
 import "package:flutter/foundation.dart";
 import "package:injectable/injectable.dart";
+import "package:package_info_plus/package_info_plus.dart";
 import "package:supabase_flutter/supabase_flutter.dart";
+import "package:universal_io/io.dart";
 
 @module
 abstract class RegisterModule {
@@ -14,10 +16,16 @@ abstract class RegisterModule {
   @singleton
   Future<SupabaseClient> client(@Named("supabase_url") String url,
       @Named("supabase_key") String key) async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    final userAgent =
+        "CopyCat/${packageInfo.version}+${packageInfo.buildNumber} (${Platform.operatingSystem}; ${Platform.operatingSystemVersion}; ${Platform.localeName}; Installer: ${packageInfo.installerStore ?? 'Unknown Store'})";
     await Supabase.initialize(
       url: url,
       anonKey: key,
       debug: kDebugMode,
+      headers: {
+        "user-agent": userAgent,
+      },
     );
     return Supabase.instance.client;
   }
